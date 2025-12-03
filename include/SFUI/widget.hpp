4231f98e40eb36890d5ded538f33fbd8977efa0f
fox102447@gmail.com <fox102447@gmail.com>
2025-12-03 10:55:16 +0300

    Now widgets have base position, getSize method

--- include/SFUI/widget.hpp
+++ include/SFUI/widget.hpp
@@ -5,16 +5,20 @@
 
 namespace sfui {
 	class Widget {
-	friend class Canvas;
 	friend class Window;
+	friend class Layout;
+	friend class DefaultLayout;
 	private:
-		sf::Vector2f m_base_offset;
-		bool is_drawable;
+		sf::Vector2f m_base_position;
+		bool m_is_drawable;
 
 	private:
 		virtual void draw(sf::RenderWindow* wnd) = 0;
+
+	public:
 		virtual void setPosition(sf::Vector2f position) = 0;
 		virtual sf::Vector2f getPosition() = 0;
+		virtual sf::Vector2f getSize() = 0;
 	};
 }
 #endif // !SFUI_WIDGET_HPP
