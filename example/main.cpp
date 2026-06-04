f9a3a29272ac140e62acff30d771b1e3e8e4067e
fox102447@gmail.com <fox102447@gmail.com>
2026-06-04 13:45:17 +0300

    Updated example file

--- example/main.cpp
+++ example/main.cpp
@@ -1,73 +1,63 @@
 #include <iostream>
 #include "SFUI/core.hpp"
+#include "SFUI/Label/label.hpp"
+#include "SFUI/Button/button.hpp"
+#include "SFUI/Button/check_box.hpp"
+#include "SFUI/Button/radio_button.hpp"
+#include "SFUI/ProgressBar/progress_bar.hpp"
+#include "SFUI/Range/range.hpp"
+#include "SFUI/TextBox/text_box.hpp"
+#include "SFUI/Image/image.hpp"
 #include "SFUI/Window/window.hpp"
 
 int main() {
-	sf::RenderWindow* main_wnd = new sf::RenderWindow(sf::VideoMode({ 800, 500 }), "SFUI test");
-	sf::Font font("ArialRegular.ttf"); // Create Font
-	
-	// Create Progress bar
-	sfui::ProgressBar* progress_bar = new sfui::ProgressBar();
-	progress_bar->setSize({ 200, 20 });
-	progress_bar->setValue(45);
+	sf::RenderWindow main_wnd(sf::RenderWindow(sf::VideoMode({ 800, 500 }), "SFUI test"));
+	std::shared_ptr<sfui::Label> text = std::shared_ptr<sfui::Label>(new sfui::Label("test"));
+	text->setFillColor(sf::Color::White);
+	text->setPositionX(100);
 
-	// Create Range
-	sfui::Range* range = new sfui::Range();
-	range->slider->setFillColor(sf::Color(128, 128, 128));
-	range->setPositionY(progress_bar->getPosition().y + 30);
+	std::shared_ptr<sfui::ProgressBar> progress_bar = std::shared_ptr<sfui::ProgressBar>(new sfui::ProgressBar());
+	progress_bar->setPositionY(300);
 
-	// Ceate Text
-	sfui::Text* progress_bar_value = new sfui::Text(font, "", 16);
-	progress_bar_value->setPositionX(progress_bar->getSize().x + 10);
-	progress_bar_value->setFillColor(sf::Color::White);
+	std::shared_ptr<sfui::Button> btn = std::shared_ptr<sfui::Button>(new sfui::Button("Click me!", [&] {
+		progress_bar->updateValue(10);
+	}));
+	btn->setPosition({ 200, 200 });
+	btn->setText("NOT CLICK!");
 
-	// Create TextBox
-	sfui::TextBox* text_box = new sfui::TextBox(new sfui::Text(font, "Enter text...", 16));
-	text_box->setSize({ 200, 50 });
+	std::shared_ptr<sfui::CheckBox> check_box = std::shared_ptr<sfui::CheckBox>(new sfui::CheckBox());
+	check_box->setPosition({ 10, 10 });
 
-	// Create Button
-	sfui::Button* btn = new sfui::Button(new sfui::Text(font, "Press", 18), [&]() {
-		std::cout << text_box->getText() << std::endl;
-	});
-	btn->setPositionY(30);
+	std::shared_ptr<sfui::RadioButton> radio_btn = std::shared_ptr<sfui::RadioButton>(new sfui::RadioButton());
+	radio_btn->setPosition({ 50, 10 });
 
-	// Create Window
-	sfui::Window* wnd = new sfui::Window(new sfui::Text(font, "Test window", 16));
-	wnd->setPosition({ 200, 100 });
+	std::shared_ptr<sfui::Image> image = std::shared_ptr<sfui::Image>(new sfui::Image("resources/DemoManSamurai.jpg"));
+	image->setPosition({ 100, 100 });
+	image->setSize({ 100, 50 });
 
-	// Create CheckBox and RadioButton
-	sfui::CheckBox* check_box;
-	sfui::RadioButton* radio_btn;
+	std::shared_ptr<sfui::Range> range = std::shared_ptr<sfui::Range>(new sfui::Range());
+	range->setPositionY(400);
 
-	check_box = new sfui::CheckBox(true, [&]() {
-		radio_btn->setStatus(false);
-		wnd->hideWindow(false);
-	});
-	check_box->setPosition({ 10, 70 });
+	std::shared_ptr<sfui::TextBox> input = std::shared_ptr<sfui::TextBox>(new sfui::TextBox("Enter something here..."));
 
-	radio_btn = new sfui::RadioButton(false, [&]() {
-		check_box->setStatus(false);
-		wnd->hideWindow(true);
-	});
-	radio_btn->setPosition({ 50, 70 });
-
-	wnd->layout->add({ btn, text_box }); // Add button and text box to window layout
-
+	std::shared_ptr<sfui::Window> window = std::shared_ptr<sfui::Window>(new sfui::Window("Simple Window"));
+	window->addChild(input);
 	// Create Canvas to display all UI
-	sfui::Canvas canvas(sf::Vector2f(main_wnd->getSize()));
-	canvas.layout->add({ progress_bar, progress_bar_value, range, check_box, radio_btn });
-	canvas.layout->add(wnd);
-	// canvas.layout->hideLayout(true);
-	while (main_wnd->isOpen()) {
-		while (const auto event = main_wnd->pollEvent()) {
-			if (event->is<sf::Event::Closed>()) main_wnd->close();
-			canvas.processEvents(*event); // Handle canvas Events
+	sfui::Canvas canvas(sf::Vector2f(main_wnd.getSize()));
+	canvas.addChild(text);
+	canvas.addChild({ btn, check_box, radio_btn });
+	canvas.addChild({ progress_bar, range, image } );
+	canvas.addChild(window);
+
+	while (main_wnd.isOpen()) {
+		while (const auto event = main_wnd.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) main_wnd.close();
+			canvas.processEvents(event.value());
 		}
-		main_wnd->clear(sf::Color(25, 25, 25));
-		canvas.draw(main_wnd); // Draw Canvas
-		// Your code goes here...
-		progress_bar->setValue(range->getValue());
-		progress_bar_value->setText(std::to_string(static_cast<int>(progress_bar->getValue())));
-		main_wnd->display();
+		main_wnd.clear(sf::Color(25, 25, 25));
+		canvas.update(main_wnd);
+		//std::cout << range->getValue() << std::endl;
+		main_wnd.draw(canvas);
+		main_wnd.display();
 	}
 }
